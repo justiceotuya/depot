d1bfb191f3a4bd7f29199a15b82adf5aea6bcb86
Justice Otuya <jotuya2@gmail.com>
2024-04-01 16:06:24 +0100

    add product page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
+  validates :title, length: { minimum: 10, message:"should exceed 10 characters" }
   validates :price, numericality: { greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: {
     case_sensitive: false
@@ -8,4 +9,5 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png)\z}i,
     message: 'must be a URL for GIF, JPG or PNG image'
   }
+
 end
